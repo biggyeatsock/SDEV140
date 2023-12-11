c755fdb68da01103137e7d2ead26bf79589ffea2
Logan Luper <loganluper6678@gmail.com>
2023-12-11 14:16:20 -0500

    final

---
+++ SDEV-140-Final.py
@@ -0,0 +1,22 @@
+###################################
+#
+#   Author: Logan Luper
+#   December 12th, 2023
+#   This program will parse a file and gather data from it to display 
+#   the number of doctor visits.
+#
+###################################
+FILE = 'final_program_input-1.dat'
+
+
+def GetUserInput():
+    usinput = input("Enter an account number:")
+    return usinput
+
+def main():
+    GetUserInput()
+    
+    
+
+
+main()
